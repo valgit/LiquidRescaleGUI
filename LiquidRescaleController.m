f20ec22ed8b220fdd18de582ebe2a8cdacabcbdb
valgit <vbrasseur@gmail.com>
2009-07-30 06:06:43 +0000

    * some code cleanup/optim

--- LiquidRescaleController.m
+++ LiquidRescaleController.m
@@ -1625,98 +1625,87 @@ NSBitmapImageRep *mask_rep;
 
 - (void) brushShapeCenterAt:(NSPoint)pt
 {
-        double mRadius = [mBrushSizeSlider doubleValue];
+	double mRadius = [mBrushSizeSlider doubleValue];
 	double mWeight = [mBrushWeightSlider doubleValue];
 	MLogString(1 ,@"radius : %f w: %f",mRadius, mWeight);
 	// Create the shape of the tip of the brush. Code currently assumes the bounding
 	//      box of the shape is square (height == width)
 	NSRect mainOval = { { pt.x, pt.y } , { 2 * mRadius, 2 * mRadius } };
-
-
-	switch ([mMaskToolButton selectedSegment]) {
-	case 0 : // retain
-		[_retainColor set];
-		break;
-	case 1 : // removal
-		[_removalColor set];
-		break;
-	case 3 : // clear
-		[_clearColor set];
-		break;
-	}
-
+	
+	
+	
 	[[NSBezierPath bezierPathWithOvalInRect:mainOval] fill];
-
+	
 }
 
 - (float)stampBrushfrom:(NSPoint)startPoint 
-	to:(NSPoint)endPoint leftOverDistance:(float)leftOverDistance
+					 to:(NSPoint)endPoint leftOverDistance:(float)leftOverDistance
 {
-        // Set the spacing between the stamps. By trail and error, I've
-        //      determined that 1/10 of the brush width (currently hard coded to 20)
-        //      is a good interval.
-        //float spacing = CGImageGetWidth(mask) * 0.1;
-        float spacing = [mBrushSizeSlider doubleValue] * 0.1;
-
-        // Anything less that half a pixel is overkill and could hurt performance.
-        if ( spacing < 0.5 )
-                spacing = 0.5;
-
-        // Determine the delta of the x and y. This will determine the slope
-        //      of the line we want to draw.
-        float deltaX = endPoint.x - startPoint.x;
-        float deltaY = endPoint.y - startPoint.y;
-
-        // Normalize the delta vector we just computed, and that becomes our step increment
-        //      for drawing our line, since the distance of a normalized vector is always 1
-        float distance = sqrt( deltaX * deltaX + deltaY * deltaY );
-        float stepX = 0.0;
-        float stepY = 0.0;
-        if ( distance > 0.0 ) {
-                float invertDistance = 1.0 / distance;
-                stepX = deltaX * invertDistance;
-                stepY = deltaY * invertDistance;
-       }
-
-        float offsetX = 0.0;
-        float offsetY = 0.0;
-
-        // We're careful to only stamp at the specified interval, so its possible
-        //      that we have the last part of the previous line left to draw. Be sure
-        //      to add that into the total distance we have to draw.
-        float totalDistance = leftOverDistance + distance;
-
-        // While we still have distance to cover, stamp
-        while ( totalDistance >= spacing ) {
-                // Increment where we put the stamp
-                if ( leftOverDistance > 0 ) {
-                        // If we're making up distance we didn't cover the last
-                        //      time we drew a line, take that into account when calculating
-                        //      the offset. leftOverDistance is always < spacing.
-                        offsetX += stepX * (spacing - leftOverDistance);
-                        offsetY += stepY * (spacing - leftOverDistance);
-
-                        leftOverDistance -= spacing;
-                } else {
-                        // The normal case. The offset increment is the normalized vector
-                        //      times the spacing
-                        offsetX += stepX * spacing;
-                        offsetY += stepY * spacing;
-                }
-
-                // Calculate where to put the current stamp at.
-                NSPoint stampAt = NSMakePoint(startPoint.x + offsetX, startPoint.y + offsetY);
-
-                // Ka-chunk! Draw the image at the current location
-                [self brushShapeCenterAt:stampAt];
-
-                // Remove the distance we just covered
-                totalDistance -= spacing;
-        }
-
-        // Return the distance that we didn't get to cover when drawing the line.
-        //      It is going to be less than spacing.
-        return totalDistance;
+	// Set the spacing between the stamps. By trail and error, I've
+	//      determined that 1/10 of the brush width (currently hard coded to 20)
+	//      is a good interval.
+	//float spacing = CGImageGetWidth(mask) * 0.1;
+	float spacing = [mBrushSizeSlider doubleValue] * 0.1;
+	
+	// Anything less that half a pixel is overkill and could hurt performance.
+	if ( spacing < 0.5 )
+		spacing = 0.5;
+	
+	// Determine the delta of the x and y. This will determine the slope
+	//      of the line we want to draw.
+	float deltaX = endPoint.x - startPoint.x;
+	float deltaY = endPoint.y - startPoint.y;
+	
+	// Normalize the delta vector we just computed, and that becomes our step increment
+	//      for drawing our line, since the distance of a normalized vector is always 1
+	float distance = sqrt( deltaX * deltaX + deltaY * deltaY );
+	float stepX = 0.0;
+	float stepY = 0.0;
+	if ( distance > 0.0 ) {
+		float invertDistance = 1.0 / distance;
+		stepX = deltaX * invertDistance;
+		stepY = deltaY * invertDistance;
+	}
+	
+	float offsetX = 0.0;
+	float offsetY = 0.0;
+	
+	// We're careful to only stamp at the specified interval, so its possible
+	//      that we have the last part of the previous line left to draw. Be sure
+	//      to add that into the total distance we have to draw.
+	float totalDistance = leftOverDistance + distance;
+	
+	// While we still have distance to cover, stamp
+	while ( totalDistance >= spacing ) {
+		// Increment where we put the stamp
+		if ( leftOverDistance > 0 ) {
+			// If we're making up distance we didn't cover the last
+			//      time we drew a line, take that into account when calculating
+			//      the offset. leftOverDistance is always < spacing.
+			offsetX += stepX * (spacing - leftOverDistance);
+			offsetY += stepY * (spacing - leftOverDistance);
+			
+			leftOverDistance -= spacing;
+		} else {
+			// The normal case. The offset increment is the normalized vector
+			//      times the spacing
+			offsetX += stepX * spacing;
+			offsetY += stepY * spacing;
+		}
+		
+		// Calculate where to put the current stamp at.
+		NSPoint stampAt = NSMakePoint(startPoint.x + offsetX, startPoint.y + offsetY);
+		
+		// Ka-chunk! Draw the image at the current location
+		[self brushShapeCenterAt:stampAt];
+		
+		// Remove the distance we just covered
+		totalDistance -= spacing;
+	}
+	
+	// Return the distance that we didn't get to cover when drawing the line.
+	//      It is going to be less than spacing.
+	return totalDistance;
 }
 
 - (void) imageDisplayViewMouseDown:(NSEvent*)event inView:(NSView*)view;
@@ -1724,29 +1713,41 @@ NSBitmapImageRep *mask_rep;
 	MLogString(1 ,@"");
 	if (_imageMask != nil ) {
 		NSPoint loc = [view convertPoint:[event locationInWindow] fromView:view];
-
+		
 		//NSLog(@"%s loc sel : %f %f",__PRETTY_FUNCTION__,loc.x,loc.y);
 		NSPoint anchor = [_panelImageView anchor];
 		//NSLog(@"%s anchor sel : %f %f",__PRETTY_FUNCTION__,anchor.x,anchor.y);
 		loc.x += anchor.x;
 		loc.y += anchor.y;
 		//NSLog(@"%s loc sel after : %f %f",__PRETTY_FUNCTION__,loc.x,loc.y);
-
+		
 		[_imageMask lockFocus];
 		//[_imageMask lockFocusOnRepresentation:mask_rep];
 		[NSGraphicsContext saveGraphicsState];
 		//[NSGraphicsContext setCurrentContext:[NSGraphicsContext
         //                graphicsContextWithBitmapImageRep:mask_rep]];
-
+		
+		switch ([mMaskToolButton selectedSegment]) {
+			case 0 : // retain
+				[_retainColor set];
+				break;
+			case 1 : // removal
+				[_removalColor set];
+				break;
+			case 3 : // clear
+				[_clearColor set];
+				break;
+		}
+		
 		[self brushShapeCenterAt:loc];	
-
+		
 		//[_imageMask recache];
 		[NSGraphicsContext restoreGraphicsState];
 		[_imageMask unlockFocus];
-
-	        mLastPoint = loc;
-        	mLeftOverDistance = 0.0;
-
+		
+		mLastPoint = loc;
+		mLeftOverDistance = 0.0;
+		
 		// very inefficient
 		[view setNeedsDisplay:YES];
 	}
@@ -1757,24 +1758,36 @@ NSBitmapImageRep *mask_rep;
 	MLogString(1 ,@"");
 	if (_imageMask !=  NULL) {
 		NSPoint loc = [view convertPoint:[event locationInWindow] fromView:view];
-
+		
 		NSPoint anchor = [_panelImageView anchor];
 		//NSLog(@"%s anchor sel : %f %f",__PRETTY_FUNCTION__,anchor.x,anchor.y);
 		loc.x += anchor.x;
 		loc.y += anchor.y;
-
+		
 		[_imageMask lockFocus];
 		[NSGraphicsContext saveGraphicsState];
 		//[NSGraphicsContext setCurrentContext:[NSGraphicsContext
-          //              graphicsContextWithBitmapImageRep:mask_rep]];
-
+		//              graphicsContextWithBitmapImageRep:mask_rep]];
+		
+		switch ([mMaskToolButton selectedSegment]) {
+			case 0 : // retain
+				[_retainColor set];
+				break;
+			case 1 : // removal
+				[_removalColor set];
+				break;
+			case 3 : // clear
+				[_clearColor set];
+				break;
+		}
+		
 		//[self brushShapeCenterAt:loc];	
 		mLeftOverDistance = [self stampBrushfrom:mLastPoint to:loc leftOverDistance:mLeftOverDistance];
-
+		
 		//[_imageMask recache];
 		[NSGraphicsContext restoreGraphicsState];
 		[_imageMask unlockFocus];
-
+		
 		mLastPoint = loc;
 		// very inefficient
 		[view setNeedsDisplay:YES];
@@ -1790,20 +1803,32 @@ NSBitmapImageRep *mask_rep;
 		//NSLog(@"%s anchor sel : %f %f",__PRETTY_FUNCTION__,anchor.x,anchor.y);
 		loc.x += anchor.x;
 		loc.y += anchor.y;
-
+		
 		[NSGraphicsContext saveGraphicsState];
 		[_imageMask lockFocus];
-
+		
+		switch ([mMaskToolButton selectedSegment]) {
+			case 0 : // retain
+				[_retainColor set];
+				break;
+			case 1 : // removal
+				[_removalColor set];
+				break;
+			case 3 : // clear
+				[_clearColor set];
+				break;
+		}
+		
 		// Stamp the brush in a line, from the last mouse location to the current one
 		mLeftOverDistance = [self stampBrushfrom:mLastPoint to:loc leftOverDistance:mLeftOverDistance];
-
+		
 		[_imageMask unlockFocus];
 		[_imageMask recache];
 		[NSGraphicsContext restoreGraphicsState];
-
+		
 		mLastPoint = NSZeroPoint;
 		mLeftOverDistance = 0.0;
-
+		
 		// very inefficient
 		[view setNeedsDisplay:YES];
 	}
@@ -1818,7 +1843,7 @@ NSBitmapImageRep *mask_rep;
 -(NSString*)previewfilename:(NSString *)file
 {
       NSString *tempFilename = [self temppath]; // NSTemporaryDirectory();
-    
+     
       return [[NSString stringWithFormat:@"%@/thumb_%@",tempFilename,file] retain];
 }
 
